eeb76c4cd3f4caa7d89664c995e6814da2cb81d9
kfir marouani <kfir.marouani@my6sense.com>
2018-01-10 17:23:39 +0200

    first commit of qjs.js

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # qjs
 JavaScript librery implementation of a queue with extra power
+
+# Usage
+
+Using qjs is very simple. Just add the script tag with the relevant reference for qjs.js or qjs.min.js file.
+
+
+# Api
+
+qjs.enqueue(item) - add an item to the queue.
+qjs.dequeue() - extract an item from the queue.
+qjs.clear() - clear the queue
+qjs.size() - return queue size
+
+
+# Event
+
+qjs.onQueue(callback) - Add a call back to be executed on queue event
---
+++ qjs/qjs.js
@@ -0,0 +1,106 @@
+(function (global) {
+
+    "use strict";
+
+    function Queue() {
+
+        var queue = [];
+        var archived = [];
+        var current = null;
+
+        var lock = false;
+        var archive = false;
+        var allowDuplicates = false;
+
+        var events = {
+            onQueue: [],
+            onDequeue: [],
+            onArchive: [],
+            onDuplicate: [],
+            onLock: []
+        };
+
+        function fireEvent(eventArray,param) {
+            eventArray.forEach(function (callback) {
+                callback(param);
+            })
+        }
+
+
+        this.enqueue = function (item) {
+
+            if (lock){
+
+                fireEvent(events.onLock,item);
+
+            } else if (!allowDuplicates && queue.indexOf(item) > -1) {
+
+                fireEvent(events.onDuplicate,item);
+
+            } else {
+
+                queue.push(item);
+
+                fireEvent(events.onQueue,item);
+            }
+
+            return this;
+        };
+
+        this.dequeue = function () {
+
+            current = queue.shift();
+
+            fireEvent(events.onDequeue,current);
+
+            if (archive) {
+                this.archive();
+            }
+
+            return current;
+        };
+
+        this.clear = function () {
+            queue = [];
+        };
+
+        this.current = function () {
+            return current;
+        };
+
+        this.archive = function () {
+            if (current) {
+                archived.push(current);
+                fireEvent(events.onArchive,current);
+            }
+        };
+
+        this.lock = function () {
+            lock = true;
+        };
+
+        this.unlock = function () {
+            lock = false;
+        };
+
+        this.size = function () {
+            return queue.length;
+        };
+
+        this.last = function () {
+            if (queue.length) {
+                return queue[queue.length - 1];
+            } else {
+                return this.current();
+            }
+        };
+
+        this.onQueue = function (callback) {
+            events.onQueue.push(callback);
+        }
+
+    }
+
+    global.qjs = new Queue();
+
+})(window || global);
